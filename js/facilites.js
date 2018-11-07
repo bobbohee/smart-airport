740bd53d06afe5497156519afd5d3d075d3a4ee3
park-bohee <nogcha04@naver.com>
2018-11-07 13:17:18 +0900

    not success

---
+++ js/facilites.js
@@ -0,0 +1,30 @@
+$(function () {
+  var key = 'qQyghynMjWVFQFaN3ZiF1neNgFaug%2FlVHSJQUgaXaBgNcG2dRjiAPV%2F0qFNoiUCcntIghxOYfmsHVMV6tbQUnw%3D%3D';
+
+  var url = 'http://openapi.airport.kr/openapi/service/FacilitiesInformation/getFacilitesInfo';
+  url += '?' + encodeURIComponent('ServiceKey') + '=' + key;
+  url += '&' + encodeURIComponent('ServiceKey') + '=' + encodeURIComponent(key);
+  url += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
+  url += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100');
+  url += '&' + encodeURIComponent('lang') + '=' + encodeURIComponent('K');
+
+  $.get(url, function (res) {
+    console.log(res);
+    var items = $(res).find("item");
+    for (let i = 0; i < items.length; i++) {
+      var facilitynm = $(items[i]).find("facilitynm").text();   /* 가게 이름 */
+      var lcnm = $(items[i]).find("lcnm").text();               /* 위치 */
+      var servicetime = $(items[i]).find("servicetime").text(); /* 시간 */
+
+      var li = "<li class='ui-first-child ui-last-child'>" +
+                  "<a href='#' rel='external' class='ui-btn ui-btn-icon-right ui-icon-carat-r'>" + facilitynm + "<br>" + servicetime + "<br>" + lcnm + "</a>" +
+                "</li>";
+      $("#facilites ul").append(li);
+    }
+  });
+
+  $("#facilites ul li a").click(function() {
+
+  });
+
+});
